51d0ea77db37dbaf31ee218f193e38bf63c6698a
Saurabh Chikhlonde <Saurabhchikhlonde@gmail.com>
2025-07-05 01:41:33 +0530

    final working changes for main menu

--- RGB_Controller/RGB_Controller.ino
+++ RGB_Controller/RGB_Controller.ino
@@ -1,12 +1,11 @@
 #include <Wire.h>
+#include <EEPROM.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <AudioTools.h>
+#include <BluetoothA2DPSink.h>
 
-#include "AudioTools.h"
-#include "BluetoothA2DPSink.h"
-
-
-// OLED Display
+// OLED
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 #define OLED_RESET    -1
@@ -17,12 +16,21 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 #define BTN_RIGHT  33
 #define BTN_SAVE   27
 
-//  Main Menu logic
-enum MenuState { MENU_AUDIO_MODE, MENU_RGB_CONFIG, MENU_NUM_STRIPS, MENU_NUM_LEDS };
+// Menu states
+enum MenuState {
+  MENU_AUDIO_MODE,
+  MENU_NUM_STRIPS,
+  MENU_LED_COUNT
+};
 MenuState currentMenu = MENU_AUDIO_MODE;
-int audioMode = 0; // 0: FFT, 1: Beat Detection
-int numStrips = 1;
-int ledsPerStrip = 30;
+
+// Config
+#define MAX_STRIPS 5
+#define MAX_LEDS_PER_STRIP 300
+uint8_t audioMode = 0; // 0 = FFT, 1 = Beat Detection
+uint8_t numStrips = 1;
+uint8_t ledsPerStrip[MAX_STRIPS] = {60, 60, 60, 60, 60};
+uint8_t currentStrip = 0;
 
 unsigned long splashStartTime;
 bool splashDone = false;
@@ -36,11 +44,14 @@ const unsigned char music_logo [] PROGMEM = {
 // A2DP + I2S
 I2SStream i2s;
 BluetoothA2DPSink a2dp_sink(i2s);
-
-// Define I2S pins
 #define I2S_BCK  25
 #define I2S_WS   22
 #define I2S_DOUT 26
+
+// EEPROM addresses
+#define ADDR_AUDIO_MODE 0
+#define ADDR_NUM_STRIPS 1
+#define ADDR_LEDS_START 10
 
 // Button debounce
 bool readButton(uint8_t pin) {
@@ -60,8 +71,27 @@ bool readButton(uint8_t pin) {
   return false;
 }
 
+// EEPROM
+void saveToEEPROM() {
+  EEPROM.write(ADDR_AUDIO_MODE, audioMode);
+  EEPROM.write(ADDR_NUM_STRIPS, numStrips);
+  for (uint8_t i = 0; i < MAX_STRIPS; i++) {
+    EEPROM.write(ADDR_LEDS_START + i, ledsPerStrip[i]);
+  }
+  EEPROM.commit();
+}
 
-// Splash screen
+void loadFromEEPROM() {
+  audioMode = EEPROM.read(ADDR_AUDIO_MODE);
+  numStrips = EEPROM.read(ADDR_NUM_STRIPS);
+  if (numStrips < 1 || numStrips > MAX_STRIPS) numStrips = 1;
+  for (uint8_t i = 0; i < MAX_STRIPS; i++) {
+    ledsPerStrip[i] = EEPROM.read(ADDR_LEDS_START + i);
+    if (ledsPerStrip[i] == 0 || ledsPerStrip[i] > MAX_LEDS_PER_STRIP) ledsPerStrip[i] = 60;
+  }
+}
+
+// Splash
 void showSplash() {
   display.clearDisplay();
   display.drawBitmap((SCREEN_WIDTH - 16) / 2, 10, music_logo, 16, 16, WHITE);
@@ -73,7 +103,7 @@ void showSplash() {
   splashStartTime = millis();
 }
 
-// Menu drawing
+// Menu UI
 void drawMenu() {
   display.clearDisplay();
   display.setTextSize(1);
@@ -84,30 +114,24 @@ void drawMenu() {
       display.setCursor(0, 0);
       display.println("Audio Mode");
       display.setCursor(10, 20);
-      display.println(audioMode == 0 ? "> FFT" : "> Beat Detection");
-      break;
-
-    case MENU_RGB_CONFIG:
-      display.setCursor(0, 0);
-      display.println("RGB Config");
-      display.setCursor(10, 20);
-      display.println("> Press SAVE");
+      display.println(audioMode == 0 ? "> FFT" : "> Beat Detect");
       break;
 
     case MENU_NUM_STRIPS:
       display.setCursor(0, 0);
-      display.println("Set Num Strips");
+      display.println("Number of Strips");
       display.setCursor(10, 20);
       display.print("> ");
       display.print(numStrips);
       break;
 
-    case MENU_NUM_LEDS:
+    case MENU_LED_COUNT:
       display.setCursor(0, 0);
-      display.println("LEDs per Strip");
+      display.print("LEDs: Strip ");
+      display.print(currentStrip + 1);
       display.setCursor(10, 20);
       display.print("> ");
-      display.print(ledsPerStrip);
+      display.print(ledsPerStrip[currentStrip]);
       break;
   }
 
@@ -117,31 +141,34 @@ void drawMenu() {
 void setup() {
   Serial.begin(115200);
 
-  // Init Buttons
+  // Init buttons
   pinMode(BTN_LEFT, INPUT_PULLUP);
   pinMode(BTN_RIGHT, INPUT_PULLUP);
   pinMode(BTN_SAVE, INPUT_PULLUP);
 
+  // Init EEPROM
+  EEPROM.begin(64);
+  loadFromEEPROM();
+
   // Init OLED
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3D)) {
     Serial.println("OLED not found");
-    while (true);
+    while (1);
   }
   showSplash();
 
-  // Setup I2S pins
+  // Init I2S
   auto cfg = i2s.defaultConfig();
   cfg.pin_bck = I2S_BCK;
   cfg.pin_ws  = I2S_WS;
   cfg.pin_data = I2S_DOUT;
   i2s.begin(cfg);
 
-  // Start Bluetooth A2DP
+  // A2DP start
   a2dp_sink.start("ESP32_Music_RX");
 }
 
 void loop() {
-  // Show splash for 2 seconds
   if (!splashDone) {
     if (millis() - splashStartTime > 2000) {
       splashDone = true;
@@ -150,33 +177,52 @@ void loop() {
     return;
   }
 
-  // Button logic
   if (readButton(BTN_LEFT)) {
     switch (currentMenu) {
-      case MENU_AUDIO_MODE: audioMode = !audioMode; break;
-      case MENU_NUM_STRIPS: if (numStrips > 1) numStrips--; break;
-      case MENU_NUM_LEDS:   if (ledsPerStrip > 1) ledsPerStrip--; break;
-      default: break;
+      case MENU_AUDIO_MODE:
+        audioMode = !audioMode;
+        break;
+      case MENU_NUM_STRIPS:
+        if (numStrips > 1) numStrips--;
+        break;
+      case MENU_LED_COUNT:
+        if (ledsPerStrip[currentStrip] > 10) ledsPerStrip[currentStrip] -= 10;
+        break;
     }
     drawMenu();
   }
 
   if (readButton(BTN_RIGHT)) {
     switch (currentMenu) {
-      case MENU_AUDIO_MODE: audioMode = !audioMode; break;
-      case MENU_NUM_STRIPS: if (numStrips < 10) numStrips++; break;
-      case MENU_NUM_LEDS:   if (ledsPerStrip < 300) ledsPerStrip++; break;
-      default: break;
+      case MENU_AUDIO_MODE:
+        audioMode = !audioMode;
+        break;
+      case MENU_NUM_STRIPS:
+        if (numStrips < MAX_STRIPS) numStrips++;
+        break;
+      case MENU_LED_COUNT:
+        if (ledsPerStrip[currentStrip] < MAX_LEDS_PER_STRIP) ledsPerStrip[currentStrip] += 10;
+        break;
     }
     drawMenu();
   }
 
   if (readButton(BTN_SAVE)) {
     switch (currentMenu) {
-      case MENU_AUDIO_MODE: currentMenu = MENU_RGB_CONFIG; break;
-      case MENU_RGB_CONFIG: currentMenu = MENU_NUM_STRIPS; break;
-      case MENU_NUM_STRIPS: currentMenu = MENU_NUM_LEDS; break;
-      case MENU_NUM_LEDS:   currentMenu = MENU_AUDIO_MODE; break;
+      case MENU_AUDIO_MODE:
+        currentMenu = MENU_NUM_STRIPS;
+        break;
+      case MENU_NUM_STRIPS:
+        currentStrip = 0;
+        currentMenu = MENU_LED_COUNT;
+        break;
+      case MENU_LED_COUNT:
+        currentStrip++;
+        if (currentStrip >= numStrips) {
+          saveToEEPROM();
+          currentMenu = MENU_AUDIO_MODE;
+        }
+        break;
     }
     drawMenu();
   }
